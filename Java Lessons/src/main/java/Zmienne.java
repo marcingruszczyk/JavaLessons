e80cade09ed8cd082bec131dcee8517c7e64ad5d
Marcin Gruszczyk <marcin@Marcins-Mini.Dlink>
2024-07-30 11:07:15 +0200

    Learn about variables

---
+++ Java Lessons/src/main/java/Zmienne.java
@@ -0,0 +1,16 @@
+public class Zmienne {
+
+
+    public static void main(String[] args) {
+        int firstNumber, secondNumber, result;
+
+        String name = "Marcin";
+        firstNumber = 1;
+        secondNumber = 2;
+        result = firstNumber + secondNumber;
+
+        name = "Piotr";
+        System.out.println(name);
+        System.out.println(result);
+    }
+}
